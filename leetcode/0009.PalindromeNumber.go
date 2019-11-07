374c5eb0e13990206562618e85f5c75993783823
Yam Zhang <yam.zhang@shopee.com>
2019-11-07 20:56:58 +0800

    update

--- leetcode/0009.PalindromeNumber.go
+++ leetcode/0009.PalindromeNumber.go
@@ -1,6 +1,29 @@
 package leetcode
 
-func isPalindromeNumber(x int) bool {
+import "fmt"
 
+func isPalindromeNumber(x int) bool {
+	if x < 0 {
+		return false
+	}
+	// uint64 length is 20
+	l := make([]int, 20)
+	length := 0
+	for i := x; i > 0; i = i / 10 {
+		l[length] = i % 10
+		length++
+	}
+	fmt.Printf("x=%d, l:%v, length=%d\n", x, l, length)
+	index := length - 1
+	if index <= 0 {
+		return true
+	}
+	for j := 0; j < index; j++ {
+		fmt.Printf("[%d]=%d, [%d]=%d\n", j, l[j], index-j, l[index-j])
+		if l[j] != l[index-j] {
+			return false
+		}
+	}
+	fmt.Println("pass")
 	return true
 }
--- leetcode/0009.PalindromeNumber_test.go
+++ leetcode/0009.PalindromeNumber_test.go
@@ -3,7 +3,7 @@ package leetcode
 import "testing"
 
 func Test_isPalindromeNumber(t *testing.T) {
-	if rst := isPalindromeNumber(-10); rst {
+	if rst := isPalindromeNumber(1211); rst {
 		t.Fatalf("%v", rst)
 	}
 	t.Log("ok!")
