6a4dcff949c097c13cfddd43f468a127f2ae3b75
byyam <yanzhang.scut@gmail.com>
2019-11-28 22:22:27 +0800

    mergeTwoLists

--- leetcode/0021.mergeTwoLists.go
+++ leetcode/0021.mergeTwoLists.go
@@ -1,1 +1,29 @@
 package leetcode
+
+import . "github.com/byyam/go-leetcode-toolkit"
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	l0 := &ListNode{Val: -1}
+	p0 := l0
+	p1 := l1
+	p2 := l2
+	for p1 != nil || p2 != nil {
+		if p1 == nil {
+			p0.Next = p2
+			p2 = p2.Next
+		} else if p2 == nil {
+			p0.Next = p1
+			p1 = p1.Next
+		} else {
+			if p1.Val < p2.Val {
+				p0.Next = p1
+				p1 = p1.Next
+			} else {
+				p0.Next = p2
+				p2 = p2.Next
+			}
+		}
+		p0 = p0.Next
+	}
+	return l0.Next
+}
--- leetcode/0021.mergeTwoLists_test.go
+++ leetcode/0021.mergeTwoLists_test.go
@@ -1,1 +1,22 @@
 package leetcode
+
+import (
+	toolkit "github.com/byyam/go-leetcode-toolkit"
+	"testing"
+)
+
+func Test_mergeTwoLists(t *testing.T) {
+	l1 := &toolkit.ListNode{Val: -1}
+	l1.AppendList([]int{1, 2, 4})
+	l1.Next.String()
+	l2 := &toolkit.ListNode{Val: -1}
+	l2.AppendList([]int{1, 3, 4})
+	l2.Next.String()
+
+	l0 := mergeTwoLists(l1.Next, l2.Next)
+	l0.String()
+	if ok := l0.CompareList([]int{1, 1, 2, 3, 4, 4}); !ok {
+		t.Fatalf("compare list error!")
+	}
+	t.Log("ok!")
+}
