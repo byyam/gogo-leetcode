08324f524b281557e6ff85793d0d2b1312cd1ef3
byyam <yanzhang.scut@gmail.com>
2020-03-30 22:55:19 +0800

    lastRemaining

--- others/62.lastRemaining.go
+++ others/62.lastRemaining.go
@@ -1,6 +1,10 @@
 package others
 
 func lastRemaining(n int, m int) int {
-
-	return 0
+	// f(N, M) = (f(N-1, M) + M)%N
+	end := 0
+	for i := 2; i <= n; i++ {
+		end = (end + m) % i
+	}
+	return end
 }
